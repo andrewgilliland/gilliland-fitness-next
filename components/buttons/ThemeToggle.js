b3fe7906c313b3f989d6e3df6db5618255938177
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-05-20 11:29:14 -0500

    updates themetoggle styles

--- components/buttons/ThemeToggle.js
+++ components/buttons/ThemeToggle.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { useTheme } from "next-themes";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
+  const [clicked, setClicked] = useState(false);
+
+  function toggle() {
+    setTheme(theme === "dark" ? "light" : "dark");
+    setClicked(!clicked);
+  }
 
   return (
-    <button className="h-5 w-5 bg-purple-100 rounded-full" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-      
-    </button>
+    <div className="relative w-10 h-4 bg-gray-400 rounded-full shadow-inner">
+      <button
+        className={`dot absolute w-6 h-6 bg-white rounded-full shadow -top-1 transition-transform transform ${
+          clicked ? `translate-x-5` : `-translate-x-1`
+        }`}
+        onClick={toggle}
+      ></button>
+    </div>
   );
 }
