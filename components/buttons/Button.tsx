ac7012d4602dca9044cfb4ef60e885c316560860
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-06-08 14:37:03 -0500

    converts button to typescript

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,8 +1,15 @@
-export default function Button({ type, roll, secondary, children }) {
+type Props = {
+  type?: "button" | "submit" | "reset";
+  role?: string;
+  secondary?: boolean;
+  children: React.ReactNode;
+};
+
+export default function Button({ type, role, secondary, children }: Props) {
   return (
     <button
       type={type}
-      roll={roll}
+      role={role}
       className={`inline-flex items-center justify-center py-2 px-4 border border-transparent shadow text-md font-semibold rounded-md  transition-opacity duration-300  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 ${
         secondary
           ? `bg-white text-purple-600 hover:bg-purple-50`
