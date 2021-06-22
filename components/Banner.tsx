1189a3cdea47781e1c1f39f31a7b7302f90498b8
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-06-22 18:10:58 -0500

    adds toggle to banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+
 export default function Banner() {
+  const [isOpen, setOpen] = useState(true);
+
+  function toggle() {
+    setOpen(!isOpen);
+  }
+
+  function toggleKeyPress() {
+    setOpen(!isOpen);
+  }
+
   return (
-    <div className="bg-purple-800 dark:bg-purple-600 w-full fixed bottom-0 right-0">
+    <div
+      className={`bg-purple-800 dark:bg-purple-600 w-full fixed bottom-0 right-0 z-10 transition ${
+        isOpen ? "" : "z-0 ease-in opacity-0 scale-90"
+      }`}
+    >
       <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between flex-wrap">
           <div className="w-0 flex-1 flex items-center">
             <span className="flex p-2 rounded-lg bg-purple-600 dark:bg-purple-800">
-              
               <svg
                 className="h-6 w-6 text-white"
                 xmlns="http://www.w3.org/2000/svg"
@@ -40,10 +55,11 @@ export default function Banner() {
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
+              onClick={toggle}
+              onKeyPress={toggleKeyPress}
               className="-mr-1 flex p-2 rounded-md hover:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
             >
               <span className="sr-only">Dismiss</span>
-              {/* <!-- Heroicon name: outline/x --> */}
               <svg
                 className="h-6 w-6 text-white"
                 xmlns="http://www.w3.org/2000/svg"
