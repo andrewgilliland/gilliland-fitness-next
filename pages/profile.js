e963719fcb2fa9b75bea141c677627ae30a3ca7f
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-07-14 19:12:54 -0500

    adds client.js sign-in.js profile.js

---
+++ pages/profile.js
@@ -0,0 +1,3 @@
+import { useState, useEffect } from "react";
+import { supabase } from "../client";
+import { useRouter } from "next/router";
---
+++ pages/sign-in.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+import { supabase } from "../client";
+
+export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+ 
+  async function signIn() {
+    const { error, data } = await supabase.auth.signIn({
+      email,
+    });
+
+    if (error) {
+      console.log({ error });
+    } else {
+      setSubmitted(true);
+    }
+  }
+
+  if (submitted) {
+    return (
+      <div>
+        <h1>Please check your email to sign in.</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <main>
+        <h1>Sign In</h1>
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          style={{ margin: 10 }}
+        />
+        <button onClick={() => signIn()}>Sign In</button>
+      </main>
+    </div>
+  );
+}
