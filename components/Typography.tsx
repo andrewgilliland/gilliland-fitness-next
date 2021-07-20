053ee801986aa6ee36d8eab019397065c362c4eb
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-07-20 18:44:46 -0500

    adds typography component

---
+++ components/Typography.tsx
@@ -0,0 +1,5 @@
+
+export function GradientHeading({children}) {
+
+    return <h2 className="text-4xl block bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-purple-800">{children}</h2>
+}
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -15,11 +15,12 @@ export default function Hero() {
           <span className="block text-gray-900 dark:text-gray-50 xl:inline">
             Lorem ipsum dolor sit{" "}
           </span>
-          <span className="block xl:inline bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-purple-600">
+          <span className="block xl:inline text-purple-600">
             amet consectetur.
           </span>
         </h1>
 
+        
         <p className="mt-3 text-base text-gray-500 dark:text-gray-400 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
           cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
